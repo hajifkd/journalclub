0bf02b5ad86dd518daa580a084278b96a2777f21
Hajime Fukuda <hajime.fukuda@me.com>
2016-06-06 01:13:05 +0900

    added proper ObjId

--- js/main.js
+++ js/main.js
@@ -12,9 +12,6 @@ function escapeHtml(content) {
   });
 }
 
-const ObjectId = (m = Math, d = Date, h = 16, s = s => m.floor(s).toString(h)) =>
-    s(d.now() / 1000) + ' '.repeat(h).replace(/./g, () => s(m.random() * h));
-
 function newTalk() {
   return {
     id: ObjectId(),
