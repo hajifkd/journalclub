3b6278c7258766ced2b069bb74c9631ef0b23e4b
Hajime Fukuda <hajime.fukuda@me.com>
2016-06-06 00:49:14 +0900

    generate obj id

--- js/main.js
+++ js/main.js
@@ -12,9 +12,12 @@ function escapeHtml(content) {
   });
 }
 
+const ObjectId = (m = Math, d = Date, h = 16, s = s => m.floor(s).toString(h)) =>
+    s(d.now() / 1000) + ' '.repeat(h).replace(/./g, () => s(m.random() * h));
+
 function newTalk() {
   return {
-    id: '000000000000000000000000',
+    id: ObjectId(),
     title: '',
     abstract: '',
     date: new Date(),
